9f1fb93f337fd04c6fbabd51f3247d3d7ce89a31
梅哲源 <2840386371@qq.com>
2025-12-04 16:08:59 +0800

    update

--- stu-web/vite.config.ts
+++ stu-web/vite.config.ts
@@ -1,21 +1,23 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  build: {
-    outDir: '../src/main/resources/static'  // 修改这一行
-  },
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
+      '@': resolve(__dirname, 'src')
+    }
   },
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
